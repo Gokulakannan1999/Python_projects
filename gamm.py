1f4761e7526b2bc3aa7883faae8da4ce929919ac
Gokulakannan1999 <91973556+Gokulakannan1999@users.noreply.github.com>
2021-10-06 17:13:45 +0530

    Banking application

---
+++ gamm.py
@@ -0,0 +1,16 @@
+a=int(input("Enter first value"))
+b=int(input("Enter second value"))
+Choice=int(input("Which option you want  1. + 2. - 3. / 4. * 5. %"))
+switcher(Choice)
+case1: '+'
+print("Addition value is : ",a+b)
+#break
+case2: '-'
+print("Subtraction value is : ",a-b)
+#break
+case3: '*'print("Multiplication value is : ",a*b)
+#break
+case4: '/'print("Division value is : ",a/b)
+#break
+case5: '%'print("Modulas value is : ",a%b)
+#break
